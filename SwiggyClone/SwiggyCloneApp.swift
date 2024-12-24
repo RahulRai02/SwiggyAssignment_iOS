cc506c5580a41318c69022d8994b56c5ede88826
Rahul Rai <rahul@Rahuls-MacBook-Air-2.local>
2024-12-24 15:11:39 +0530

    Initial Commit

---
+++ SwiggyClone/SwiggyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiggyCloneApp.swift
+//  SwiggyClone
+//
+//  Created by Rahul Rai on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiggyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
